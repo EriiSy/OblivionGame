76dc3526a1c529743bc5ee483c54c51a6ceb6956
Eri <erysy77@gmail.com>
2024-11-13 14:35:12 -0300

    Add Class Menu and WindowGame, Modified Main, implements Action Listener

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,33 +1,12 @@
 package main;
 
-import javax.swing.*;
-import java.net.URL;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        JFrame window = new JFrame();
-        
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Para fechar o programa quando clicarmos em "x"
-        window.setResizable(false); // Podemos redimensionar a janela
-        window.setTitle("Oblivion Souls");
-        URL iconURL = Main.class.getResource("/res/icons/Logo.png");
-        //iconURL is null when not found
-        ImageIcon icon = new ImageIcon(iconURL);
-        window.setIconImage(icon.getImage());
-
-
-        GamePanel gamePanel = new GamePanel(); //Criando o painel
-        window.add(gamePanel);
-
-        window.pack(); // Como essa janela deve ser dimensionada, ela se ajustará ao tamanho e layout preferidos de seus subcomponentes.
-
-        window.setLocationRelativeTo(null); // A janela será exibida no centro da tela. Sem especificar a localização
-        window.setVisible(true); // Para poder ver a tela.
-
-        gamePanel.startGameThread();
-
+        WindowGame windowGame = new WindowGame();
+        Menu menu = new Menu();
+        menu.startMenu();
+        windowGame.gameStartThread();
     }
 }
 
---
+++ src/main/Menu.java
@@ -0,0 +1,46 @@
+package main;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.net.URL;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Menu extends JFrame implements ActionListener {
+
+    JButton BStart = new JButton("Start Game");
+    JButton BExit = new JButton("Exit Game");
+
+    private JPanel MenuPanel() {
+        JPanel panel = new JPanel();
+        panel.add(BStart);
+        panel.add(BExit);
+
+        return panel;
+    }
+
+    public Menu(){
+        this.setSize(800, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Para fechar o programa quando clicarmos em "x"
+        this.setResizable(true); 
+        this.setTitle("Oblivion Souls");
+        URL iconURL = Main.class.getResource("/res/icons/Logo.png");
+    }
+
+    public void startMenu(){
+        this.add(MenuPanel());
+        this.setLocationRelativeTo(null); // A janela será exibida no centro da tela. Sem especificar a localização
+        this.setVisible(true); // Para poder ver a tela.
+    }
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.startMenu();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    
+}
