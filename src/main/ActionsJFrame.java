be5b8848e0786a9a1870765b7a398a5926fbc6bd
Jonhyyplay <jonathancleyber@acad.ifma.edu.br>
2024-11-26 15:22:34 -0300

    Método para verificar se as imagens dos botões carregarão corretamente.

--- src/main/ActionsJFrame.java
+++ src/main/ActionsJFrame.java
@@ -10,7 +10,6 @@ import java.awt.Dimension;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
-
 public class ActionsJFrame extends JFrame implements ActionListener {
     private JPanel panel = new JPanel();
     WindowGame game = new WindowGame();
@@ -22,16 +21,16 @@ public class ActionsJFrame extends JFrame implements ActionListener {
     JButton BOptions, BOptionsKeys, BOptionsSound, BOptionsGraphics, BOptionsBack;
     JButton BCredits;
     JLabel LCredits, LCreditsDeveloper, LCreditsArt, LCreditsAdvisor, LCreditsSpecialThanks;
-    JLabel LNickName;
-    JTextField TFNickName; 
-    JCheckBox CHSound;
-    JRadioButton RBLegenda; 
-    ButtonGroup BGGraphics; 
-    JMenuItem MIExit, MISizeMax, MISizeMin, MICredits;
-    JMenu MFile, MOptions, MHelp; 
-    JMenuBar MBMenu; 
-    JTable TBCredits; 
-    JScrollPane SCPane;
+    // JLabel LNickName;
+    // JTextField TFNickName; 
+    // JCheckBox CHSound;
+    // JRadioButton RBLegenda; 
+    // ButtonGroup BGGraphics; 
+    // JMenuItem MIExit, MISizeMax, MISizeMin, MICredits;
+    // JMenu MFile, MOptions, MHelp; 
+    // JMenuBar MBMenu; 
+    // JTable TBCredits; 
+    // JScrollPane SCPane;
 
     public ActionsJFrame(MenuPanel mp) {
         this.mp = mp;
@@ -45,21 +44,35 @@ public class ActionsJFrame extends JFrame implements ActionListener {
         BOptionsKeys = new JButton("KEYBOARD");
         BOptionsSound = new JButton("SOUNDS");
         BOptionsGraphics = new JButton("VIDEO");
-        BOptionsBack = new JButton("BACK<-");
+        BOptionsBack = new JButton("BACK");
 
+        //Adiciona os botões ao painel
         panel.add(BStart);
         panel.add(BExit);
         panel.add(BOptions);
+
     }
   
-
-    protected void SizeIconBotton(){
-        for (JButton button : new JButton[]{BStart, BExit, BOptions, BOptionsKeys, 
-            BOptionsSound, BOptionsGraphics, BOptionsBack}) {
-            button.setIcon(resizeIcon("/res/bottons/LightBlue/Normal.png", 200, 50));
-            button.setRolloverIcon(resizeIcon("/res/bottons/LightBlue/Hover.png", 200, 50));
-            button.setPressedIcon(resizeIcon("/res/bottons/LightBlue/Clicked.png", 200, 50));
-            button.setPreferredSize(new Dimension(200, 50));
+    //Essa parte configura os ícones de diferentes estados (normal, hover, clicked) para um conjunto de botões.
+    protected void SizeIconBotton(){ //Método para redimensionar os ícones dos botões
+        for (JButton button : new JButton[]
+            {BStart, BExit, BOptions, BOptionsKeys, BOptionsSound, BOptionsGraphics, BOptionsBack} //Todos os conjuntos de botões
+            ) {
+                
+                //Aqui são definidos os ícones de cada botão
+                ImageIcon normalIcon = resizeIcon("/res/bottons/LightBlue/Normal.png", 200, 50);
+                ImageIcon hoverIcon = resizeIcon("/res/bottons/LightBlue/Hover.png", 200, 50);
+                ImageIcon clickedIcon = resizeIcon("/res/bottons/LightBlue/Clicked.png", 200, 50);
+                
+                if (normalIcon != null && hoverIcon != null && clickedIcon != null) { //Verifica se as imagens foras carregadas corretamente
+                    button.setIcon(normalIcon); //Define a imagem padrão do botão
+                    button.setRolloverIcon(hoverIcon); //Define a imagem quando o cursor está sobre o botão
+                    button.setPressedIcon(clickedIcon); //Define a imagem quando o botão é pressionado
+                } else {
+                    System.out.println("Um ou mais botões não encontrado."); //Caso não carregados, exibe uma mensagem de erro
+                }
+            
+                button.setPreferredSize(new Dimension(200, 50));
             button.setBorderPainted(false);
             button.setFocusPainted(false);
             button.setContentAreaFilled(false);
@@ -123,6 +136,7 @@ public class ActionsJFrame extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        //Implementa um método abstrato da interface ActionListener
         if (e.getSource() == BStart) {
             game.gameStartThread();
         } else if (e.getSource() == BExit) {
