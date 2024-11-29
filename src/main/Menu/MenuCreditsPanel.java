20a031d81b8fa9f604e97f29864cb13d98f9bd6d
Eri <erysy77@gmail.com>
2024-11-29 12:13:28 -0300

    Test MenuCredtisPanel Centralized

--- src/main/Menu/MenuCreditsPanel.java
+++ src/main/Menu/MenuCreditsPanel.java
@@ -1,66 +1,88 @@
 package main.Menu;
+
 import javax.swing.JPanel;
-
-import main.Menu.RenderIcons.IconsMenuRender;
-
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.Dimension;
-
 import javax.swing.BorderFactory;
 import javax.swing.Box;
-import main.Menu.ConfigMenu;
-
+import javax.swing.ImageIcon;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 
 public class MenuCreditsPanel extends JPanel {
 
-        private JLabel lblCredits,lblDeveloper,lblDesigner, lSpecialThanks;
-        ConfigMenu layout = new ConfigMenu();
+    private JLabel lblCredits, lblDeveloper, lblDesigner, lSpecialThanks;
+    private JLabel backgroundImageLabel;
+    ConfigMenu layout = new ConfigMenu();
 
-        public MenuCreditsPanel() {
-            this.setLayout(layout.createCreditsPanelLayout(this));
-            this.setBorder(BorderFactory.createEmptyBorder(50, 0, 50, 0));
-            setOpaque(false);
-            Labels();
-            configLabels();
-            configPopUP();
-            addCreditsPanel();
+    public MenuCreditsPanel() {
+        this.setLayout(new GridBagLayout());
+        this.setBorder(BorderFactory.createEmptyBorder(50, 0, 50, 0));
+        setOpaque(false);
+        Labels();
+        configLabels();
+        configPopUP();
+        addCreditsPanel();
+        addBackgroundImage();
+    }
 
-        }
-
-        public void configPopUP() {
-            lSpecialThanks.addMouseListener(new java.awt.event.MouseAdapter() {
-                public void mouseClicked(java.awt.event.MouseEvent evt) {
-                    javax.swing.JOptionPane.showMessageDialog(null, "OBRIGADO!");
-                }
-            });
-        }
-
-        public void Labels() {
-            lblCredits = new JLabel("CREDITS");
-            lblDeveloper = new JLabel("DEVELOPER: ---");
-            lblDesigner = new JLabel("DESIGNER:----");
-            lSpecialThanks = new JLabel("Special Thanks");
-
-        }
-
-        public void configLabels() {
-            for (JLabel label : new JLabel[]{lblCredits, lblDeveloper, lblDesigner, lSpecialThanks}) {
-                label.setFont(new Font("Monocraft", Font.BOLD, 20));
-                label.setForeground(Color.WHITE);
+    public void configPopUP() {
+        lSpecialThanks.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                javax.swing.JOptionPane.showMessageDialog(null, "OBRIGADO!");
             }
+        });
+    }
+
+    public void Labels() {
+        lblCredits = new JLabel("CREDITS");
+        lblDeveloper = new JLabel("DEVELOPER: ---");
+        lblDesigner = new JLabel("DESIGNER:----");
+        lSpecialThanks = new JLabel("Special Thanks");
+    }
+
+    public void configLabels() {
+        for (JLabel label : new JLabel[]{lblCredits, lblDeveloper, lblDesigner, lSpecialThanks}) {
+            label.setFont(new Font("Monocraft", Font.BOLD, 20));
+            label.setForeground(Color.WHITE);
         }
-        public void addCreditsPanel() {
-            this.add(Box.createRigidArea(new Dimension(0, 200)));
-            this.add(Box.createVerticalGlue());
-            for (JLabel label : new JLabel[]{lblCredits, lblDeveloper, lblDesigner, lSpecialThanks}) {
-                label.setAlignmentX(layout.x); 
-                label.setAlignmentY(layout.y);
-                this.add(label);
-                this.add(Box.createVerticalStrut(10));
-            }
-            this.add(Box.createVerticalGlue());
+    }
+
+    public void addCreditsPanel() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new java.awt.Insets(10, 0, 10, 0);
+
+        this.add(Box.createRigidArea(new Dimension(0, 200)), gbc);
+        gbc.gridy++;
+        this.add(Box.createVerticalGlue(), gbc);
+        for (JLabel label : new JLabel[]{lblCredits, lblDeveloper, lblDesigner, lSpecialThanks}) {
+            label.setAlignmentX(layout.x);
+            label.setAlignmentY(layout.y);
+            gbc.gridy++;
+            this.add(label, gbc);
+            gbc.gridy++;
+            this.add(Box.createVerticalStrut(10), gbc);
         }
+        gbc.gridy++;
+        this.add(Box.createVerticalGlue(), gbc);
+    }
+
+    public void addBackgroundImage() {
+        ImageIcon backgroundImage = new ImageIcon("path/to/your/image.png");
+        backgroundImageLabel = new JLabel(backgroundImage);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.gridheight = GridBagConstraints.REMAINDER;
+        gbc.anchor = GridBagConstraints.CENTER;
+        this.add(backgroundImageLabel, gbc);
+    }
 }
-
